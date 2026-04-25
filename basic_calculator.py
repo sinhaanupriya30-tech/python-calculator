8d255b2117d034eb911c211e829eead8889b1f26
sinhaanupriya30-tech <sinhaanupriya30@gmail.com>
2026-04-25 22:43:32 +0530

    Update basic_calculator.py

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,15 +1,25 @@
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
+while True:
+    print("\n--- Calculator ---")
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    
+    print("1. Add\n2. Subtract\n3. Multiply\n4. Divide")
+    choice = input("Choose option (1-4): ")
 
-op = input("Choose operation (+, -, *, /): ")
+    if choice == "1":
+        print("Result:", a + b)
+    elif choice == "2":
+        print("Result:", a - b)
+    elif choice == "3":
+        print("Result:", a * b)
+    elif choice == "4":
+        if b != 0:
+            print("Result:", a / b)
+        else:
+            print("Cannot divide by zero")
+    else:
+        print("Invalid choice")
 
-if op == "+":
-    print("Result:", a + b)
-elif op == "-":
-    print("Result:", a - b)
-elif op == "*":
-    print("Result:", a * b)
-elif op == "/":
-    print("Result:", a / b)
-else:
-    print("Invalid operation")
+    cont = input("Do you want to continue? (y/n): ")
+    if cont.lower() != "y":
+        break
